0de78c6dff82b0f842e46c0828b1e1ced8de2c0f
Usta Eren Julian <erenjulian.usta@students.bfh.ch>
2022-06-29 22:44:35 -0500

    V2.0

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
 function compute()
 {
+    //Get the values and calculate 
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var interest = principal * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
     p = document.getElementById("principal").value;
+    //Interest Rate Text
+    document.getElementById("result").innerHTML = "Interest : If you deposit <mark>" + principal + "</mark>,<br/>" +
+        "at an interest rate of <mark>" + rate + "</mark>,<br/>" +
+        "You will receive an amount of <mark>" + interest + "</mark>,<br/> " +
+        "in the year <mark>" + year + "</mark>";
 }
 
+//Update ther rate value using a slider
 function updateRate() 
 {
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText=rateval;
 }
    
+//Positive Values Checker 
 function validatePrincipal() {
     var principal = document.getElementById("principal").value;
     var biggerThanZero = parseInt(principal) > 0;

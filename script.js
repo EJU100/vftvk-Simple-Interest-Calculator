ad6c4abb34006fff404a999b60d71e0acfdf5737
Usta Eren Julian <erenjulian.usta@students.bfh.ch>
2022-06-29 21:55:26 -0500

    V1.1

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 function compute()
 {
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
     p = document.getElementById("principal").value;
-    
 }
-        
+
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
+   
+function validatePrincipal() {
+    var principal = document.getElementById("principal").value;
+    var biggerThanZero = parseInt(principal) > 0;
+    if (!biggerThanZero) {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+    }
+
+}
